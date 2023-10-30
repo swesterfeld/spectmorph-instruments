1b159053d91550a3bfba40b2a39f259b448257f6
Stefan Westerfeld <stefan@space.twc.de>
2023-10-30 11:38:37 +0100

    Update tools: create directories as needed, improve error handling.
    Signed-off-by: Stefan Westerfeld <stefan@space.twc.de>

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ test -d "$1" || die "instrument directory $1 doesn't exist"
 cd "$1"
 pwd
 rm -f ../tmp/build-$1.zip
-zip -r -q ../tmp/build-$1.zip .
+zip -r -q ../tmp/build-$1.zip . || die "unable to build zip file for instrument $1"
 cd ..
 smwavset build tmp/build-$1.zip instruments/standard/${1}.smset || die "unable to build new instrument $1"
 smtool instruments/standard/${1}.smset strip-all > /dev/null
